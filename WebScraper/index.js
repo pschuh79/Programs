edaf3b8b569ec1eb296b844b9ef60ba0cd179b58
Phil <pschuh79@gmail.com>
2022-03-12 21:33:47 -0600

    finished scraper

--- WebScraper/index.js
+++ WebScraper/index.js
@@ -6,7 +6,26 @@ const express = require('express');
 
 const app = express();
 
-app.listen(PORT, () => console.log('server running on PORT ${ PORT }'));
+const url = 'https://www.theguardian.com/us';
+
+axios(url)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const articles = []
+
+        $('.fc-item__title', html).each(function () {
+            const title = $(this).text()
+            const url = $(this).find('a').attr('href')
+            articles.push({
+                title,
+                url
+            })
+        })
+        console.log(articles)
+    }).catch(err => console.log(err))
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
 
 
 
